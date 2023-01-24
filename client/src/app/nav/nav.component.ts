1c57922de250a41bc2a2b3a1153c59d6c6b99d2d
Alexander Sidorenko <asiddev@protonmail.com>
2023-01-23 19:32:11 -0800

    conditional render when logged in- not final security

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../__services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: (error) => console.log(error),
+    });
   }
 
+  logout() {
+    this.loggedIn = false;
+  }
 }
