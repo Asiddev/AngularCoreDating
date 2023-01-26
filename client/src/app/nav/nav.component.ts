b86ff9d20e4a801ebde3d79a314cdd45d3bb46a7
Alexander Sidorenko <asiddev@protonmail.com>
2023-01-26 10:41:02 -0800

    conditional rendering for fake login, non persistant, and rename folder to appropriate name

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../__services/account.service';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
